0ff8de2e0d455bfe2cfcd71b12bfe7d628c78ba4
Eduardo Gordillo <edu.m.gordillo@gmail.com>
2022-03-31 11:31:49 +0200

    css changes on form menu

--- src/components/menuList/MenuList.jsx
+++ src/components/menuList/MenuList.jsx
@@ -40,7 +40,7 @@ const MenuList = ({ newProduct }) => {
   return (
     <>
       <ul className="list">
-        <h2>Lista de productos</h2>
+        <h2>Menú</h2>
         {products.map((product) => {
           return (
             <li key={product._id}>
@@ -63,7 +63,3 @@ const MenuList = ({ newProduct }) => {
 
 export default MenuList;
 
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="trasparent" class="bi bi-x-lg" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M13.854 2.146a.5.5 0 0 1 0 .708l-11 11a.5.5 0 0 1-.708-.708l11-11a.5.5 0 0 1 .708 0Z" />
-  <path fill-rule="evenodd" d="M2.146 2.146a.5.5 0 0 0 0 .708l11 11a.5.5 0 0 0 .708-.708l-11-11a.5.5 0 0 0-.708 0Z" />
-</svg>
